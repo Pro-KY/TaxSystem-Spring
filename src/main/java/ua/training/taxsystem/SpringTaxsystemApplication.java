39d8c803cf1ce38c50ef6ae10a9fcb1250aaffb5
Yuriy <klyuyko.yu@gmail.com>
2019-10-09 01:26:05 +0300

    create basic project structure

---
+++ src/main/java/ua/training/taxsystem/SpringTaxsystemApplication.java
@@ -0,0 +1,13 @@
+package ua.training.taxsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaxsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaxsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/training/taxsystem/SpringTaxsystemApplicationTests.java
@@ -0,0 +1,16 @@
+package ua.training.taxsystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTaxsystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
