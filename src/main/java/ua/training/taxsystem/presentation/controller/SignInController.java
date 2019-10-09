ca4b5d225cd785bb8c37f8c7b49710344c6f7b2d
Yuriy <klyuyko.yu@gmail.com>
2019-10-09 18:35:05 +0300

    add log4j2 dependency

--- src/main/java/ua/training/taxsystem/presentation/controller/SignInController.java
+++ src/main/java/ua/training/taxsystem/presentation/controller/SignInController.java
@@ -1,17 +1,18 @@
 package ua.training.taxsystem.presentation.controller;
 
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ua.training.taxsystem.util.properties.ViewProperties;
 
 import static ua.training.taxsystem.util.properties.ViewProperties.PATH_INDEX;
-
+@Log4j2
 @Controller
 public class SignInController {
     @GetMapping("/")
     public String showForm() {
-        System.out.println("I'm here");
+        log.info("I'm here");
         return ViewProperties.getViewPath(PATH_INDEX);
     }
 }
